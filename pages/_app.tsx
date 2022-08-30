084c656cb762bbc3128bfe86e46cd901611591b8
joél <joel@joel.fm>
2022-08-30 03:03:42 -0700

    layout, fix terrible typo in path alias

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import { ApolloProvider } from '@apollo/client'
 import { AppProps } from 'next/app'
 import { useApollo } from '@/lib/apollo-client'
+import { Layout } from '@/components/layout'
 
 export default function App({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps)
 
   return (
     <ApolloProvider client={apolloClient}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ApolloProvider>
   )
 }
